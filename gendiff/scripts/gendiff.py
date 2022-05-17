c0cc1791f20cef30351f207debfb76ec48934bd8
Nikita Badin <nikitabadin@MacBook-Pro-Nikita.local>
2022-05-17 12:21:05 +0300

    init project and added basic CLI functionality

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,35 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        usage='gendiff [-h] [-f FORMAT] first_file second_file',
+        description='Generate diff'
+    )
+
+    parser.add_argument(
+         'first_file',
+         metavar='first_file',
+         type=str,
+    )
+
+    parser.add_argument(
+         'second_file',
+         metavar='second_file',
+         type=str,
+    )
+
+    parser.add_argument(
+         '-f',
+         '--format',
+         action='store',
+         help='set format of output',
+    )
+
+    args = parser.parse_args()
+    print(args)
+
+
+if __name__ == '__main__':
+    main()
